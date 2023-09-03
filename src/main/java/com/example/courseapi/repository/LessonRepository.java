79840254ead6502d3aad777a549b6b0130c97ea8
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2023-09-03 12:03:40 +0300

    lessonRepository code refactored

---
+++ src/main/java/com/example/courseapi/repository/LessonRepository.java
@@ -0,0 +1,4 @@
+package com.example.courseapi.model;
+
+public interface LessonRepository extends org.springframework.data.jpa.repository.JpaRepository<com.example.courseapi.model.Lesson, java.lang.String> {
+}
--- src/main/java/com/example/courseapi/repository/lessonRepository.java
+++
@@ -1,2 +0,0 @@
-package com.example.courseapi.repository;public interface lessonRepository {
-}
